a875febd9a9d025813ede61c902792f0fe3ff2af
howardhow656 <howard91010468@gmail.com>
2026-04-11 17:59:30 +0800

    '增加database'

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 from fastapi import FastAPI, Form, APIRouter
 import tensorflow as tf
-from router import process
+from router import process, storage
 
 
 app = FastAPI()
@@ -12,7 +12,7 @@ app = FastAPI()
 
 
 app.include_router(process.router, prefix="/process", tags=['Model'])
-
+app.include_router(storage.router, prefix="/storage", tags=['Storage'])
 
 
 
